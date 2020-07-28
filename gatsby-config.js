bb5bcc2135ff294e5cbaf5dba2dda8fa7789b1cb
David Rozmajzl <dmrozmajzl@gmail.com>
2020-07-27 23:58:26 -0400

    First Commit

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	siteMetadata: {
+    description: "Personal page of David Rozmajzl",
+    locale: "en",
+    title: "David Rozmajzl",
+  },
+  plugins: [
+    {
+      resolve: "@wkocjan/gatsby-theme-intro",
+      options: {
+        theme: "bright-green",
+      },
+    },
+  ],
+}
